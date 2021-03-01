0c53fcb0a2724e716f0545348a81ee82384075b2
Nikita <45769418+kimpors@users.noreply.github.com>
2021-03-01 23:46:00 +0200

    Прототип , в якій я ввожу номер поля і потім  результат виводиться.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -10,6 +10,28 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            char[] field = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            // Знак для игрока
+            const char X = 'X';
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Clear();
+
+                // Вивод поля
+                Console.WriteLine($"{field[0]} | {field[1]} | {field[2]}");
+                Console.WriteLine($"{field[3]} | {field[4]} | {field[5]}");
+                Console.WriteLine($"{field[6]} | {field[7]} | {field[8]}");
+
+                byte result;
+                byte.TryParse(Console.ReadKey(true).KeyChar.ToString(), out result);
+
+                if (result > 0)
+                {
+                    field[result - 1] = X;
+                }
+            }
         }
     }
 }
