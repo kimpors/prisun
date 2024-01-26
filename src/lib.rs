ca0c5645111cfdbbd8308d4830e15e55fa6de9c2
kimpors <mikita_korshak@ukr.net>
2024-01-26 10:04:13 +0200

    Move all logic to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,222 @@
+use std::fmt;
+use termion::{color, terminal_size};
+
+pub enum State {
+    Win,
+    Lose,
+    Draw,
+    None,
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            State::Win => write!(f, "Win"),
+            State::Lose => write!(f, "Lose"),
+            State::Draw => write!(f, "Draw"),
+            State::None => write!(f, ""),
+        }
+    }
+}
+
+
+pub struct Game {
+    size: u8,
+    field: Vec<Vec<char>>,
+    state: State,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {
+            size: 3,
+            state: State::None,
+            field: vec![vec!['1', '2', '3'],
+                        vec!['4', '5', '6'],
+                        vec!['7', '8', '9']],
+        }
+    }
+
+    pub fn set(&mut self, index: u8) -> Result<(), String> {
+        let size = &self.size;
+        let field = &mut self.field;
+
+        if index < 1 || index as u8 > size * size {
+            panic!("Wrong argument, expects from 1 to {}", size * size);
+        }
+
+        let mut row = 0;
+        let mut col = index as usize;
+
+        while col > 3 {
+            col -= 3;
+            row += 1;
+        }
+
+        let curr = &mut field[row][col - 1];
+
+        match curr {
+            'x' => (),
+            'o' => (),
+            _ => *curr = 'x',
+        }
+
+        Ok(())
+    }
+
+    pub fn bot_move(&mut self) {
+        let size = self.size as usize;
+
+        match &mut self.field[1][1]{
+            'x' => (),
+            'o' => (),
+            other => {
+                *other = 'o';
+                return;
+            }
+        }
+
+        let mut wins: Vec<(usize, usize)> = Vec::new();
+
+
+        for y in 0..size {
+            for x in 0..size {
+                match self.field[y][x] {
+                    'o' => (),
+                    'x' => (),
+                    other => {
+                        self.field[y][x] = 'o';
+
+                        if let State::Lose = self.check_win() {
+                            self.field[y][x] = other;
+                            wins.push((y, x));
+                            continue;
+                        }
+
+                        self.field[y][x] = other;
+                    },
+                }
+            }
+        }
+
+        if !wins.is_empty() {
+
+            let w = wins[0];
+            self.field[w.0][w.1] = 'o';
+            return;
+        }
+
+        for row in &mut self.field {
+            for col in row {
+                match col {
+                    'x' => (),
+                    'o' => (),
+                    _ => {
+                        *col = 'o';
+                        return;
+                    },
+                }
+            }
+        }
+    }
+
+
+    pub fn check_win(&mut self) -> State {
+        let win = vec!['x', 'x', 'x'];
+        let lose = vec!['o', 'o', 'o'];
+
+        // Vertical Pattern
+        for row in self.field.as_slice() {
+            if row.as_slice() == win {
+                self.state = State::Win;
+                return State::Win;
+            } else if row.as_slice() == lose {
+                self.state = State::Lose;
+                return State::Lose;
+            }
+        }
+
+        // Horizontal Pattern
+        for i in 0..self.size as usize {
+            let horizontal: &[char] = &[self.field[0][i], self.field[1][i], self.field[2][i]];
+
+            if horizontal == win {
+                self.state = State::Win;
+                return State::Win;
+            } else if horizontal == lose {
+                self.state = State::Lose;
+                return State::Lose;
+            }
+        }
+
+        // Diagonal Pattern
+        for i in 0..2 {
+            let diagonal: &[char] = &[self.field[0][i * 2], self.field[1][1], self.field[2][2 - i * 2]];
+            if diagonal == win {
+                self.state = State::Win;
+                return State::Win;
+            } else if diagonal == lose {
+                self.state = State::Lose;
+                return State::Lose;
+            }
+        }
+
+        // Check For A Draw
+        for row in &self.field {
+            for ceil in row {
+                if ceil.is_numeric() {
+                    return State::None;
+                }
+            }
+        }
+
+        self.state = State::Draw;
+        return State::Draw;
+    }
+
+
+    pub fn draw(&self) {
+        let offset = self.get_offset();
+
+        let pos = termion::cursor::Goto(1, offset.1);
+        let clear = termion::clear::All;
+
+        println!("{clear}{pos}");
+
+        // Header
+        match &self.state {
+            State::None => (),
+            other => {
+                print!("{}", color::Fg(color::Yellow));
+                println!("{}{}{}", "\t".repeat(offset.0 as usize),  " ".repeat(8 - other.to_string().len()), other);
+                println!("{}", color::Fg(color::Reset));
+            } 
+        }
+
+        print!("{}", color::Fg(color::Blue));
+
+
+        // Main Field
+        for row in &self.field {
+            print!("{}", "\t".repeat(offset.0 as usize));
+
+            for (i, ceil) in row.iter().enumerate() {
+                if i == row.len() - 1 {
+                    print!(" {ceil} ");
+                } else {
+                    print!(" {ceil} | ");
+                }
+            }
+
+            println!("\n{}{}", "\t".repeat(offset.0.into()), "-".repeat(13));
+        }
+
+        print!("{}", color::Fg(color::Red));
+    }
+
+    fn get_offset(&self) -> (u16, u16) {
+        let size = terminal_size().unwrap();
+
+        return (size.0 / 16, size.1 / 2);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,195 +1,13 @@
-extern crate termion;
-
-use termion::color; 
-use std::{fmt, io::{self, Write}};
-use termion::terminal_size;
-
-const SIZE: usize = 3;
-
-enum GameState {
-    Win,
-    Lose,
-    Draw,
-    None,
-}
-
-impl fmt::Display for GameState {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            GameState::Win => write!(f, "Win"),
-            GameState::Lose => write!(f, "Lose"),
-            GameState::Draw => write!(f, "Draw"),
-            GameState::None => write!(f, ""),
-        }
-    }
-}
-
-fn center_offset(cols: &u16) -> String {
-    let mut offset = String::new();
-
-    for _ in 0..cols / 16 {
-        offset.push('\t');
-    }
-
-    return offset;
-}
-
-
-fn render(field: [[char; SIZE]; SIZE], state: &GameState) {
-    let size = terminal_size().unwrap();
-    let offset = center_offset(&size.0);
-
-    println!("{}", size.1);
-
-    let pos = termion::cursor::Goto(1, size.1 / 2);
-    let clear = termion::clear::All;
-
-    println!("{clear}{pos}");
-
-
-    // Header
-    match state {
-        GameState::None => (),
-        s => {
-            print!("{}", color::Fg(color::Yellow));
-            println!("{offset}{}{}", " ".repeat(8 - s.to_string().len()), s);
-            println!("{}", color::Fg(color::Reset));
-        } 
-    }
-
-    print!("{}", color::Fg(color::Blue));
-
-    // Main Field
-    for row in field {
-        print!("{offset}");
-
-        for (i, ceil) in row.iter().enumerate() {
-            if i == row.len() - 1 {
-                print!(" {ceil} ");
-            } else {
-                print!(" {ceil} | ");
-            }
-        }
-
-        println!("\n{offset}{}", "-".repeat(13));
-    }
-
-    print!("{}", color::Fg(color::Red));
-}
-
-fn check_win(field: [[char; SIZE]; SIZE]) -> GameState {
-    let win: [char; SIZE] = ['x', 'x', 'x'];
-    let lose: [char; SIZE] = ['o', 'o', 'o'];
-
-    // Vertical Pattern
-    for &row in field.as_slice() {
-        if row == win {
-            return GameState::Win;
-        } else if row == lose {
-            return GameState::Lose;
-        }
-    }
-
-    // Horizontal Pattern
-    for i in 0..SIZE {
-        let horizontal: &[char] = &[field[0][i], field[1][i], field[2][i]];
-
-        if horizontal == win {
-            return GameState::Win;
-        } else if horizontal == lose {
-            return GameState::Lose;
-        }
-    }
-
-    // Diagonal Pattern
-    for i in 0..2 {
-        let diagonal: &[char] = &[field[0][i * 2], field[1][1], field[2][2 - i * 2]];
-        if diagonal == win {
-            return GameState::Win;
-        } else if diagonal == lose {
-            return GameState::Lose;
-        }
-    }
-
-    // Check For A Draw
-    for row in field {
-        for ceil in row {
-            if ceil.is_numeric() {
-                return GameState::None;
-            }
-        }
-    }
-
-    return GameState::Draw;
-}
-
-fn bot_move(field: &mut [[char; SIZE]; SIZE]) {
-    match field[1][1] {
-        'x' => (),
-        'o' => (),
-        _ => {
-            field[1][1] = 'o';
-            return;
-        }
-    }
-
-    let mut wins: Vec<(usize, usize)> = Vec::new();
-
-    for y in 0..SIZE {
-        for x in 0..SIZE {
-            match field[y][x] {
-                'x' => (),
-                'y' => (),
-                c => {
-                    field[y][x] = 'o';
-
-                    match check_win(*field) {
-                        GameState::Lose => wins.push((y, x)),
-                        _ => {
-                            print!("{}-", field[y][x]);
-                            field[y][x] = c;
-                            println!("{}", field[y][x]);
-                        } ,
-                    }
-                }
-            }
-        }
-    }
-
-    if !wins.is_empty() {
-        let first = wins.first().unwrap();
-        field[first.0][first.1] = 'o';
-        return;
-    }
-
-    for row in field {
-        for ceil in row {
-            match ceil {
-                'x' => (),
-                'o' => (),
-                c => {
-                    print!("{c}");
-                    *c = 'o';
-                    return;
-                }
-            }
-        }
-    }
-}
+use std::io::{self, Write};
+use prisun::{Game, State};
 
 fn main() {
-    let mut field = [['1', '2', '3'],
-                    ['4', '5', '6'],
-                    ['7', '8', '9']];
-
-
-    let mut row: usize;
-    let mut column: usize;
     let mut buf;
+    let mut game = Game::new();
 
-    let state = loop {
+    loop {
         buf = String::new();
-        render(field, &GameState::None);
+        game.draw();
 
         print!("\nEnter free ceil: ");
         let _ = io::stdout().flush();
@@ -198,37 +16,21 @@ fn main() {
             .read_line(&mut buf)
             .unwrap();
 
-        print!("{buf}");
-
-        column = match buf.trim().parse() {
+        let index = match buf.trim().parse() {
             Ok(value) => value,
             Err(_) => continue,
         };
 
-        match column {
-            1..=9 => (),
-            _ => continue,
-        }
+        if let Err(_) =
+            game.set(index) { continue; }
 
-        row = 0;
-        while column > 3 {
-            column -= 3;
-            row += 1;
-        }
+        game.bot_move();
 
-        match field[row][column - 1] {
-            'x' => continue,
-            'o' => continue,
-            _ => field[row][column - 1] = 'x',
-        }
-
-
-        bot_move(&mut field);
-        match check_win(field) {
-            GameState::None => (),
-            value => break value,
+        match game.check_win() {
+            State::None => (),
+            _ => break,
         }
     };
 
-    render(field, &state);
+    game.draw();
 }
